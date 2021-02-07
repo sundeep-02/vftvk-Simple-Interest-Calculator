f5855dcdcc2dca395a83e83abfa8a0db56f62743
sundeep-02 <sundeepsanthan2001@gmail.com>
2021-02-07 14:22:55 +0530

    modified

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    document.getElementById("result").innerHTML = "";
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = Number(document.getElementById("years").value);
+
+    if (principal <= 0)
+    {
+        alert("Enter a positive number!");
+        document.getElementById("principal").focus();
+    }
+    else
+    {
+        var interest; 
+        interest = (principal * years * rate) / 100;
+
+        var actualyear = 2021 + years;
+
+        document.getElementById("result").innerHTML += "If you deposit <mark>" + principal + "</mark>,";
+        document.getElementById("result").innerHTML += "<br>";
+        document.getElementById("result").innerHTML += "at an interest rate of <mark>" + rate + "%</mark>.";
+        document.getElementById("result").innerHTML += "<br>";
+        document.getElementById("result").innerHTML += "You will receive an amount of <mark>" + interest + "</mark>,";
+        document.getElementById("result").innerHTML += "<br>";
+        document.getElementById("result").innerHTML += "in the year <mark>" + actualyear + "</mark><br>";
+    }
 }
-        
+
+function updateValue(val) 
+{
+    document.getElementById("span").textContent=val;
+}
